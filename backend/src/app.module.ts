0d8033990645ea0bbbc2169614110f85f9db5381
toannn00 <134350869+toannn00@users.noreply.github.com>
2025-02-10 17:55:06 +0700

    Add video and auth controllers and services
    - Add video controller/service
    - Add auth controller/service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VideoController } from './video/controller/video.controller';
+import { VideoService } from './video/service/video.service';
+import { AuthController } from './auth/controller/auth.controller';
+import { AuthService } from './auth/service/auth.service';
 
 @Module({
   imports: [
@@ -9,7 +13,7 @@ import { AppService } from './app.service';
       isGlobal: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, VideoController, AuthController],
+  providers: [AppService, VideoService, AuthService],
 })
 export class AppModule {}
