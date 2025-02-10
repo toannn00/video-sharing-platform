ac37ca40420a26ab51d0b4f0cc301f5dcbfb0f36
toannn00 <134350869+toannn00@users.noreply.github.com>
2025-02-10 18:10:57 +0700

    Add video module

--- backend/src/video/video.module.ts
+++ backend/src/video/video.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { VideoController } from './controller/video.controller';
+import { VideoService } from './service/video.service';
+import { AuthModule } from '../auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { VideoSchema } from './schema/video.schema';
 
-@Module({})
+@Module({
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([{ name: 'Video', schema: VideoSchema }]),
+  ],
+  controllers: [VideoController],
+  providers: [VideoService],
+})
 export class VideoModule {}
