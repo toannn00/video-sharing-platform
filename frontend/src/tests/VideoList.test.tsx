90c042472f9d67f3d3f5287f5a7282b01de986a0
toannn00 <134350869+toannn00@users.noreply.github.com>
2025-02-12 14:53:02 +0700

    Fix test

--- frontend/src/tests/VideoList.test.tsx
+++ frontend/src/tests/VideoList.test.tsx
@@ -18,12 +18,12 @@ const mockVideo = {
 
 const mockVideos = [mockVideo];
 
-describe("VideoItem", () => {
+describe("VideoList", () => {
   test("should render and match the snapshot", () => {
     const renderer = ShallowRenderer.createRenderer();
     renderer.render(
       <BrowserRouter>
-        <VideoList videoList={mockVideos} />
+        <VideoList videoList={mockVideos} loading={false} />
       </BrowserRouter>
     );
     const result = renderer.getRenderOutput();
